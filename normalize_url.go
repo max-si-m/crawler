8e96966669b2ddeb463baa928d9266f47d156979
Maksym Dzhulii <mak7.dj@gmail.com>
2024-09-07 21:01:08 +0300

    add more test cases

--- normalize_url.go
+++ normalize_url.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"net/url"
+	"strings"
 )
-func normalizeURL(fullUrl string) (string, error) {
-	parsedUrl, err := url.Parse(fullUrl)
+
+func normalizeURL(rawURL string) (string, error) {
+	parsedURL, err := url.Parse(rawURL)
 	if err != nil {
-		return "", err
+		return "", fmt.Errorf("couldn't parse URL: %w", err)
 	}
 
-	return parsedUrl.Host + parsedUrl.Path, nil
+	fullPath := parsedURL.Host + parsedURL.Path
+	fullPath = strings.ToLower(fullPath)
+	fullPath = strings.TrimSuffix(fullPath, "/")
+
+	return fullPath, nil
 }
